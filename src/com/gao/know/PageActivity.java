168ff6500d00d036234518fc99d31a843a3af0c9
GaoMatrix <gcquan08@gmail.com>
2015-03-01 10:31:41 +0800

    Implement slidingmenu.在third中添加需要的第三方库

--- src/com/gao/know/PageActivity.java
+++ src/com/gao/know/PageActivity.java
@@ -7,8 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.TextView;
 
 import com.gao.know.adapter.ViewPagerAdapter;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,7 @@ public class PageActivity extends Activity {
 
     private ViewPager mViewPager;
     private LocalActivityManager mLocalActivityManager;
+    private SlidingMenu mSlidingMenu;
 
     /**
      * 让ViewPager滑动activity ViewPager滑动view或者是Fragment
@@ -31,6 +34,26 @@ public class PageActivity extends Activity {
         mLocalActivityManager = new LocalActivityManager(this, true);
         mLocalActivityManager.dispatchCreate(savedInstanceState);// 必须被调用
         initAcitivities();
+        
+        initSlidingMenu();
+    }
+
+    private void initSlidingMenu() {
+        // 实例化一个slidingmenu
+        mSlidingMenu = new SlidingMenu(this);
+        mSlidingMenu.setMode(SlidingMenu.LEFT);// slidingmenu出现的屏幕左右方向
+        mSlidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);// 设置滑出slidingmenu的地方
+        mSlidingMenu.setShadowDrawable(R.drawable.shadow);// 设置主页面和slidingmenu页面之间的阴影图片
+        mSlidingMenu.setShadowWidthRes(R.dimen.shadow_width);// 设置阴影的宽度
+        mSlidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);// 设置主页面在slidingmenu上显示的宽度
+        // sm.setBehindWidth(400);//直接设置slidingmenu的宽度
+        mSlidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);// 将当前的slidingmenu作为activity的一部分显示
+
+        mSlidingMenu.setMenu(R.layout.activity_main);// 设置slidingmenu显示的layout
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText("I'm Slidind menu");
+        
+        //new NavigationHandler(this, mSlidingMenu);// 实现导航页的动画需要的代码量比较大，所以写在单独的类中。
     }
 
     /**
